89632f2bacb4d3bd612bc12ef23bb49bc29b0ba0
Ta4ilka <artem_balin_2004@mail.ru>
2020-08-18 18:00:12 +0300

    Summary
    Наработки по главной странице (нерабочая версия)

---
+++ News Storage.js
@@ -0,0 +1,37 @@
+class Storage {
+  constructor() {
+    this.items = [];
+    this.id = 1;
+  }
+
+  getList() {
+    let temps = [];
+    for (let i = 0; i <= this.items.length; i += 1) {
+      const { value, id } = this.items[i];
+      temps[i] = { value: value, id: id };
+    }
+  }
+
+  add(s) {
+    this.items.push({ value: s, id: this.id });
+    this.id += 1;
+  }
+
+  delete(id) {
+    if (this.hasId(id)) {
+      const i = this.items.findIndex(id);
+      this.items.slice(i, 1);
+      return true;
+    }
+    return false;
+  }
+
+  hasId(id) {
+    return this.items.findIndex((item) => item.id === id) >= 0;
+  }
+
+  findIndex(id) {
+    return this.items.findIndex((item) => item.id === id);
+  }
+}
+module.exports = Storage;
--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const express = require("express");
+const hbs = require("express-handlebars");
 const app = express();
+const Storage = require("./News Storage.js");
+
+app.set("view engine", "hbs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultView: "",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
+
+app.use("/static", express.static(__dirname + "/static"));
+
+const storage = new Storage();
+
+app.listen(3000);
 
 app.get("/", function (req, res) {
   res.send("hello");
 });
 
-app.listen(3000);
+app.get("/home", (req, res) => {
+  temps = storage.getList();
+  res.render("home", { list: temps });
+});
+
+app.get("/add", (req, res) => {
+  storage.add(req.body.data);
+  res.redirect("/home");
+});
+
+app.get("/delete", (req, res) => {
+  const tmpid = parseInt(req.query.id, 10);
+  if (storage.delete) {
+    res.redirect("/home");
+  } else {
+    res.status(404).send("Not found");
+  }
+});
