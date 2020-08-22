e4b1af95e0d9c2e4e96f039dae4c048ba2f1c46b
ta4ilka69 <artem_balin_2004@mail.ru>
2020-08-22 21:03:53 +0300

    все сразу
    траблы с добавлением и /info

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const hbs = require("hbs");
+const hbs = require('hbs');
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
-app.set("views", __dirname + "/views");
-const Storage = require("./news.js");
-hbs.registerPartials(__dirname + "/views/partials");
+app.set('views', __dirname + '/views');
+const Storage = require('./news.js');
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.use("/static", express.static(__dirname + "/static"));
+app.use('/static', express.static(__dirname + '/static'));
 
 const storage = new Storage();
-const pagesController = require("./pages.js");
+const pagesController = require('./pages.js');
 const Pages = new pagesController();
 
-app.get("/", function (req, res) {
-  res.send("hello");
+app.get('/', function (req, res) {
+  res.send('hello');
 });
 
-app.get("/info", Pages.info());
+//app.get('/info', Pages.info());
 
 app.listen(3000);
 
-app.get("/home", (req, res) => {
-  temps = storage.news();
-  res.render("home", { list: temps });
+app.get('/home', (req, res) => {
+  let temps = storage.news();
+  res.render('home', { list: temps });
 });
 
-app.post("/add", (req, res) => {
-  storage.add(req.body.data);
-  res.redirect("/home");
+app.post('/add', (req, res) => {
+  storage.add(req.body.header, req.body.text);
+  res.redirect('/home');
 });
 
-app.get("/add", (req, res) => {
-  res.render("addition");
+app.get('/add', (req, res) => {
+  res.render('addition');
 });
 
-app.get("/delete", (req, res) => {
-  const tmpid = parseInt(req.query.id, 10);
+app.get('/delete', (req, res) => {
+  const tmpid = parseInt(req.query.id.toString(), 10);
   if (storage.delete(tmpid)) {
-    res.redirect("/home");
+    res.redirect('/home');
   } else {
-    res.status(404).send("Not found");
+    res.status(404).send('Not found');
   }
 });
