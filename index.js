dd1a022247d580e3feda124dd7d3ddf22027ec65
ta4ilka69 <artem_balin_2004@mail.ru>
2020-08-23 19:29:46 +0300

    +редактирование + обновление меню
    и немного добавлений (новый partial, шаблоны, svg)

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const hbs = require("hbs");
+const hbs = require('hbs');
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
-app.set("views", __dirname + "/views");
-hbs.registerPartials(__dirname + "/views/partials");
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.use("/static", express.static(__dirname + "/static"));
+app.use('/static', express.static(__dirname + '/static'));
 
-const pagesController = require("./pages.js");
+const pagesController = require('./pages.js');
 const Pages = new pagesController();
 
-const Storage = require("./news.js");
+const Storage = require('./news.js');
 const storage = new Storage();
 
-app.get("/", function (req, res) {
-  res.send("hello");
+app.get('/', function (req, res) {
+  res.send('hello');
 });
 
-app.get("/info", Pages.info);
+app.get('/info', Pages.info);
 
-app.get("/home", async (req, res) => {
+app.get('/home', async (req, res) => {
   let temps = await storage.news();
-  res.render("home", { list: temps });
+  Pages.home(temps,req,res);
 });
 
-app.post("/add", (req, res) => {
+app.post('/add', (req, res) => {
   storage.add(req.body.header, req.body.text);
-  res.redirect("/home");
+  res.redirect('/home');
 });
 
-app.get("/add", (req, res) => {
-  res.render("addition");
-});
+app.get('/add', Pages.add);
 
-app.get("/delete", (req, res) => {
+app.get('/delete', (req, res) => {
   const tmpid = parseInt(req.query.id.toString(), 10);
   if (!storage.delete(tmpid)) {
-    res.status(404).send("Not found");
+    res.status(404).send('Not found');
   }
-  res.redirect("/home");
+  res.redirect('/home');
+});
+
+app.get('/edit', async (req, res) => {
+  const tmpid = parseInt(req.query.id.toString(), 10);
+  const item = await storage.itemById(tmpid);
+  const header = await item.header;
+  res.render('edit', { tmpid, header });
+});
+
+app.post('/edition', (req, res) => {
+  const tmpid = parseInt(req.query.id.toString(), 10);
+  const value = req.body.text;
+  if (!storage.edit(tmpid, value)) {
+    res.status(404).send('Not found');
+  }
+  res.redirect('/home');
 });
 
 app.listen(3000);
--- pages.js
+++ pages.js
@@ -1,6 +1,13 @@
 class Controller {
-    info(req,res) {
-    res.send('afwlja');
+  info(req, res) {
+    res.render('info');
+  }
+  add(req, res) {
+    res.render('addition');
+  }
+
+  home(list, req, res) {
+    res.render('home', { list: list });
   }
 }
 
