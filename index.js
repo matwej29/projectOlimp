e706ce5ac5792970c5590f25fca608da9d381342
ta4ilka69 <artem_balin_2004@mail.ru>
2020-08-27 22:58:27 +0300

    Prettier("")

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const hbs = require('hbs');
+const hbs = require("hbs");
 
-app.set('view engine', 'hbs');
-app.use(express.urlencoded({ extended: true }));
-app.set('views', __dirname + '/views');
-hbs.registerPartials(__dirname + '/views/partials');
+app.set("view engine", "hbs");
+app.use(express.urlencoded({extended: true}));
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.use('/static', express.static(__dirname + '/static'));
+app.use("/static", express.static(__dirname + "/static"));
 
-const pagesController = require('./pages.js');
+const pagesController = require("./pages.js");
 const Pages = new pagesController();
 
-const Storage = require('./news.js');
+const Storage = require("./news.js");
 const storage = new Storage();
 
-app.get('/', function (req, res) {
-  Pages.redirectToHome(res);
+app.get("/", function (req, res) {
+    Pages.redirectToHome(res);
 });
 
-app.get('/info', Pages.info);
+app.get("/info", Pages.info);
 
-app.get('/home', async (req, res) => {
-  let temps = await storage.news();
-  Pages.home(temps, req, res);
+app.get("/home", async (req, res) => {
+    let temps = await storage.news();
+    Pages.home(temps, req, res);
 });
 
-app.post('/add', (req, res) => {
-  storage.add(req.body.header, req.body.text);
-  Pages.redirectToHome(res);
+app.post("/add", (req, res) => {
+    storage.add(req.body.header, req.body.text);
+    Pages.redirectToHome(res);
 });
 
-app.get('/add', Pages.add);
+app.get("/add", Pages.add);
 
-app.get('/delete', (req, res) => {
-  const tmpid = parseInt(req.query.id.toString(), 10);
-  if (!storage.delete(tmpid)) {
-    Pages.status404(res);
-  }
-  Pages.redirectToHome(res);
+app.get("/delete", (req, res) => {
+    const tmpid = parseInt(req.query.id.toString(), 10);
+    if (! storage.delete(tmpid)) {
+        Pages.status404(res);
+    }
+    Pages.redirectToHome(res);
 });
 
-app.get('/edit', async (req, res) => {
-  const tmpid = parseInt(req.query.id.toString(), 10);
-  const item = await storage.itemById(tmpid);
-  const header = await item.header;
-  Pages.edit(req, res, tmpid, header);
+app.get("/edit", async (req, res) => {
+    const tmpid = parseInt(req.query.id.toString(), 10);
+    const item = await storage.itemById(tmpid);
+    const header = await item.header;
+    Pages.edit(req, res, tmpid, header);
 });
 
-app.post('/edition', (req, res) => {
-  const tmpid = parseInt(req.query.id.toString(), 10);
-  const value = req.body.text;
-  if (!storage.edit(tmpid, value)) {
-    Pages.status404(res);
-  }
-  Pages.redirectToHome(res);
+app.post("/edition", (req, res) => {
+    const tmpid = parseInt(req.query.id.toString(), 10);
+    const value = req.body.text;
+    if (! storage.edit(tmpid, value)) {
+        Pages.status404(res);
+    }
+    Pages.redirectToHome(res);
 });
 
-app.get('/organizers', (req, res) => {
-  Pages.organizers(req, res);
+app.get("/organizers", (req, res) => {
+    Pages.organizers(req, res);
 });
 
-app.get('/partners', (req,res)=>{
-  Pages.partners(req,res);
+app.get("/partners", (req, res) => {
+    Pages.partners(req, res);
 });
 
 app.listen(3000);
--- pages.js
+++ pages.js
@@ -1,34 +1,34 @@
 class Controller {
-  info(req, res) {
-    res.render('info');
-  }
-  add(req, res) {
-    res.render('addition');
-  }
+    info(req, res) {
+        res.render("info");
+    }
+    add(req, res) {
+        res.render("addition");
+    }
 
-  home(list, req, res) {
-    res.render('home', { list: list });
-  }
+    home(list, req, res) {
+        res.render("home", {list: list});
+    }
 
-  redirectToHome(res) {
-    res.redirect('/home');
-  }
+    redirectToHome(res) {
+        res.redirect("/home");
+    }
 
-  status404(res) {
-    res.status(404).send('Not found');
-  }
+    status404(res) {
+        res.status(404).send("Not found");
+    }
 
-  edit(req, res, tmpid, header) {
-    res.render('edit', { tmpid, header });
-  }
+    edit(req, res, tmpid, header) {
+        res.render("edit", {tmpid, header});
+    }
 
-  organizers(req, res) {
-    res.render('organizers');
-  }
+    organizers(req, res) {
+        res.render("organizers");
+    }
 
-  partners(req,res){
-    res.render('partners');
-  }
+    partners(req, res) {
+        res.render("partners");
+    }
 }
 
 module.exports = Controller;
