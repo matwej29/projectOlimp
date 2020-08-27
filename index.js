55ef9bf19a11b1d89a293330eecdc0538a3cec5d
Rayan4ik <51135127+MatveyScherbakow3331@users.noreply.github.com>
2020-08-27 23:46:07 +0300

    http auth
    Что с отступами?)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const hbs = require("hbs");
 
 app.set("view engine", "hbs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("views", __dirname + "/views");
 hbs.registerPartials(__dirname + "/views/partials");
 
@@ -15,54 +15,61 @@ const Pages = new pagesController();
 const Storage = require("./news.js");
 const storage = new Storage();
 
+const basicAuth = require("express-basic-auth");
+
+const auth = basicAuth({
+  users: { admin: "admin" },
+  challenge: true,
+});
+
 app.get("/", function (req, res) {
-    Pages.redirectToHome(res);
+  Pages.redirectToHome(res);
 });
 
 app.get("/info", Pages.info);
 
 app.get("/home", async (req, res) => {
-    let temps = await storage.news();
-    Pages.home(temps, req, res);
+  let temps = await storage.news();
+  Pages.home(temps, req, res);
 });
 
-app.post("/add", (req, res) => {
-    storage.add(req.body.header, req.body.text);
-    Pages.redirectToHome(res);
+app.post("/add", auth, (req, res) => {
+  storage.add(req.body.header, req.body.text);
+  Pages.redirectToHome(res);
 });
 
 app.get("/add", Pages.add);
 
 app.get("/delete", (req, res) => {
-    const tmpid = parseInt(req.query.id.toString(), 10);
-    if (! storage.delete(tmpid)) {
-        Pages.status404(res);
-    }
-    Pages.redirectToHome(res);
+  const tmpid = parseInt(req.query.id.toString(), 10);
+  if (!storage.delete(tmpid)) {
+    Pages.status404(res);
+  }
+  Pages.redirectToHome(res);
 });
 
 app.get("/edit", async (req, res) => {
-    const tmpid = parseInt(req.query.id.toString(), 10);
-    const item = await storage.itemById(tmpid);
-    const header = await item.header;
-    Pages.edit(req, res, tmpid, header);
+  const tmpid = parseInt(req.query.id.toString(), 10);
+  const item = await storage.itemById(tmpid);
+  const header = await item.header;
+  Pages.edit(req, res, tmpid, header);
 });
 
 app.post("/edition", (req, res) => {
-    const tmpid = parseInt(req.query.id.toString(), 10);
-    const value = req.body.text;
-    if (! storage.edit(tmpid, value)) {
-        Pages.status404(res);
-    }
-    Pages.redirectToHome(res);
+  const tmpid = parseInt(req.query.id.toString(), 10);
+  const value = req.body.text;
+  if (!storage.edit(tmpid, value)) {
+    Pages.status404(res);
+  }
+  Pages.redirectToHome(res);
 });
 
 app.get("/organizers", (req, res) => {
-    Pages.organizers(req, res);
+  Pages.organizers(req, res);
 });
 
 app.get("/partners", (req, res) => {
-    Pages.partners(req, res);
+  Pages.partners(req, res);
 });
 
 app.listen(3000);
